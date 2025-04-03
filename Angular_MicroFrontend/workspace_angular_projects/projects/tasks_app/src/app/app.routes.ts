f1a4006095ef4d7ad20c87500bc80dcda6a6b914
LovelyPS <123lovelyps@gmail.com>
2025-04-03 11:40:26 +0530

    Created component for tasklist APP.

--- Angular_MicroFrontend/workspace_angular_projects/projects/tasks_app/src/app/app.routes.ts
+++ Angular_MicroFrontend/workspace_angular_projects/projects/tasks_app/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { TaskListComponent } from './task-list/task-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [ // Main route for app1
+    { path: 'task_list', component: TaskListComponent }  // Example of another route
+];
